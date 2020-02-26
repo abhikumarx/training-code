c91e25b685ceaa9d08f061b5d23d9859abc25be7
abhikumarx <akumarfvtc@gmail.com>
2020-02-26 12:27:15 -0600

    initial commit

---
+++ 1_csharp/Digimon/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Digimon
+{
+
+  public class Digimon{
+
+  }
+
+  public class Calculation{
+
+  
+
+  }
+
+  public class Program 
+  {
+
+    private static void Main(string[] args)
+    {
+      var p = new Program();
+      p.Fight(null, null);
+    }
+   
+    public void Fight(Digimon a, Digimon b)
+    {
+      Console.WriteLine("{0} fights {1}", a, b);
+    }
+
+    public int Multiply(Calculation a, Calculation b)
+    {
+      Console.WriteLine("Enter a number:" , a , b);
+      return 5;
+    }
+
+  }
+}
---
+++ 1_csharp/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using sc = System.Console;
+
+namespace HelloWorld
+{
+ internal class Program
+  {
+  static void Main(string[] args)
+    {
+      sc.WriteLine("Hello World!");
+      
+    }
+  }
+}
